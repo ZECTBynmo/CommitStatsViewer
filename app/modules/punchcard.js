963fce261cb6903f44fd27255f74337a51fb3cb3
ZECTBynmo <michael.vegeto@gmail.com>
2012-11-17 00:57:24 -0500

    Get the punchcard module working

--- app/modules/punchcard.js
+++ app/modules/punchcard.js
@@ -20,22 +20,23 @@ function(app, Backbone, Repo) {
   PunchCard.Views.Item = Backbone.View.extend({
     template: "punchcard/item",
 
-    tagName: "canvas",
+    tagName: "div",
 
     data: function() {
       return { model: this.model };
     },
-    
-	changeUser: function(ev) {
-      var model = this.model;
-      var branch = model.branch;
-      var name = model.name;
-
-      app.router.go("branch", branch, "user", name);
-    },
 	
-    initialize: function() {
+    initialize: function( options ) {
+      this.committerInfo = options.model.attributes.committerInfo;
       this.model.on("change", this.render, this);
+    },
+
+    afterRender: function() {
+      var canvasElement = document.getElementById("mainCanvas");
+
+      var testUser = this.committerInfo["unknown:ZECTBynmo@.(none)"];
+
+      var graph = new PunchCardGraph( testUser );
     }
   });
 	
@@ -51,27 +52,15 @@ function(app, Backbone, Repo) {
     },
 
     beforeRender: function() {
+      var punchCardItem = new PunchCard.Model();
 
+      this.insertView("div", new PunchCard.Views.Item({
+        model: punchCardItem
+      }));
     },
 
     afterRender: function() {
-		var canvasElement = document.getElementById("mainCanvas");
-		var context = canvasElement.getContext("2d");
-		
-		var width = 125;  // Triangle Width
-		var height = 105; // Triangle Height
-		var padding = 20;
-
-		// Draw a path
-		context.beginPath();
-		context.moveTo(padding + width/2, padding);        // Top Corner
-		context.lineTo(padding + width, height + padding); // Bottom Right
-		context.lineTo(padding, height + padding);         // Bottom Left
-		context.closePath();
-
-		// Fill the path
-		context.fillStyle = "#ffc821";
-		context.fill();
+  
     },
 
     initialize: function() {
